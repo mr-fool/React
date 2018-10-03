006dc48c90155e37c624626af57356c2aec77707
mr-fool <mr-fool@users.noreply.github.com>
2018-10-02 22:34:01 -0600

    learning
    json and adding loading screen

--- robotfriends/src/App.js
+++ robotfriends/src/App.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 
 class  App extends Component{
 	constructor() {
 		super()
 		this.state = {
-				robots: robots,
+				robots: [],
 				searchfield: ''
 		}
 	}
-	
+	componentDidMount() {
+		fetch("http://jsonplaceholder.typicode.com/users")
+			.then(response => response.json())
+		.then(users => this.setState({robots: users}) )
+	}
 	onSearchChange = (event) =>{
 		this.setState({searchfield: event.target.value})
 		
@@ -22,13 +25,17 @@ class  App extends Component{
 		const filterRobots = this.state.robots.filter(robots =>{
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
-		return(
-			<div className='tc'>
-				<h1>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filterRobots} />
-			</div>
-		);
+		if (this.state.robots.length == 0) {
+			return <h1>Loading </h1>
+		} else {
+			return(
+				<div className='tc'>
+					<h1>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filterRobots} />
+				</div>
+			);
+		}
 	}
 }
 
